5d01bc5fd8c3438af622bdd37fe609fbc578bce2
liam <liamvanunu1231@gmail.com>
2023-04-24 21:06:37 +0300

    Added script that matches from all the frame descriptors
    Signed-off-by: liam <liamvanunu1231@gmail.com>

--- exe/check_matches.cc
+++ exe/check_matches.cc
@@ -27,24 +27,24 @@ int main(int argc, char **argv)
   // Load ORB_SLAM2 map and extract its descriptors
   ORB_SLAM2::System system(vocPath, droneYamlPathSlam, ORB_SLAM2::System::MONOCULAR, true, true, map_input_dir + "simulatorMap.bin", true, false);
 
-  std::vector<cv::Mat> descriptors_map_vector;
+  std::vector<cv::Mat> orb_slam_map_descriptors_vector;
   cv::Mat descriptors_map;
   for (auto& mp : system.GetMap()->GetAllMapPoints()) {
-      descriptors_map_vector.push_back(mp->GetDescriptor());
+      orb_slam_map_descriptors_vector.push_back(mp->GetDescriptor());
   }
 
   // Concatenate all the descriptors in descriptors_map
-  cv::vconcat(descriptors_map_vector, descriptors_map);
+  cv::vconcat(orb_slam_map_descriptors_vector, descriptors_map);
 
   // Set up ORB feature detector and matcher
   cv::Ptr<cv::ORB> orb = cv::ORB::create();
-  cv::BFMatcher matcher(cv::NORM_HAMMING, true);
+  cv::BFMatcher matcher(cv::NORM_HAMMING, false);
 
   // Process images in the input folder and find the one that matches the most
   std::string input_folder_path = frames_folder;
   std::vector<std::string> image_paths;
   std::vector<cv::Mat> descriptors_images;
-  std::vector<double> scores;
+  std::vector<int> scores;
   for (const auto& entry : std::filesystem::directory_iterator(input_folder_path)) {
     // Load image
     cv::Mat img = cv::imread(entry.path().string(), cv::IMREAD_GRAYSCALE);
@@ -56,41 +56,30 @@ int main(int argc, char **argv)
 
     // Match descriptors with the map
     std::vector<std::vector<cv::DMatch>> matches;
-    matcher.knnMatch(descriptors, descriptors_map, matches, 1);
+    matcher.knnMatch(descriptors, descriptors_map, matches, 2);
 
     // Find good matches
     std::vector<cv::DMatch> good_matches;
-    for (size_t i = 0; i < matches.size(); i++) {
-        if (matches[i][0].distance < 0.7 * matches[i][1].distance) {
-            good_matches.push_back(matches[i][0]);
+    for (auto& match: matches) {
+        if (match[0].distance < 0.7 * match[1].distance) {
+            good_matches.push_back(match[0]);
         }
-    }
-
-    // Compute score
-    double score = 0.0;
-    int count = 0;
-    for (const auto& match : good_matches) {
-        score += match.distance;
-        count++;
-    }
-    if (count > 0) {
-        score /= count;
     }
 
     // Save the image path and descriptors
     image_paths.push_back(entry.path().string());
     descriptors_images.push_back(descriptors);
-    scores.push_back(score);
+    scores.push_back((int)good_matches.size());
 
     // Print the score
-    std::cout << "Image: " << entry.path().string() << ", Score: " << score << std::endl;
+    std::cout << "Image: " << entry.path().string() << ", Score: " << good_matches.size() << std::endl;
   }
 
   // Find the image with the highest similarity score
   int best_index = 0;
-  double best_score = std::numeric_limits<double>::max();
+  double best_score = 0;
   for (int i = 0; i < image_paths.size(); ++i) {
-    if (scores[i] < best_score) {
+    if (scores[i] > best_score) {
       best_index = i;
       best_score = scores[i];
     }
