b45c89d9ed5b46d4805dacd0f2bf625265324ec7
liamvanunu <liamvanunu1231@gmail.com>
2023-06-09 13:15:21 +0300

    Added new script for both printing
    Signed-off-by: liamvanunu <liamvanunu1231@gmail.com>

---
+++ exe/run_model_with_orbs_and_orb_slam_map.cc
@@ -0,0 +1,326 @@
+#include <thread>
+#include <future>
+#include <queue>
+
+#include <pangolin/pangolin.h>
+#include <pangolin/geometry/geometry.h>
+#include <pangolin/gl/glsl.h>
+#include <pangolin/gl/glvbo.h>
+
+#include <pangolin/utils/file_utils.h>
+#include <pangolin/geometry/glgeometry.h>
+
+#include "include/run_model/TextureShader.h"
+#include "include/Auxiliary.h"
+
+#include <Eigen/SVD>
+#include <Eigen/Geometry>
+
+#include <opencv2/core.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/calib3d.hpp>
+
+#define NEAR_PLANE 0.1
+#define FAR_PLANE 20
+
+void drawPoints(std::vector<cv::Point3d> seen_points, std::vector<cv::Point3d> new_points_seen) {
+    std::string settingPath = Auxiliary::GetGeneralSettingsPath();
+    std::ifstream programData(settingPath);
+    nlohmann::json data;
+    programData >> data;
+    programData.close();
+
+    const int point_size = data["pointSize"];
+
+    glPointSize(point_size);
+    glBegin(GL_POINTS);
+    glColor3f(0.0, 0.0, 0.0);
+
+    for (auto point: seen_points) {
+        glVertex3f((float) (point.x), (float) (point.y), (float) (point.z));
+    }
+    glEnd();
+
+    glPointSize(point_size);
+    glBegin(GL_POINTS);
+    glColor3f(1.0, 0.0, 0.0);
+
+    for (auto point: new_points_seen) {
+        glVertex3f((float) (point.x), (float) (point.y), (float) (point.z));
+    }
+    std::cout << new_points_seen.size() << std::endl;
+
+    glEnd();
+}
+
+Eigen::Matrix4f loadMatrixFromFile(const std::string &filename) {
+    Eigen::Matrix4f matrix;
+    std::ifstream infile(filename);
+
+    if (infile.is_open()) {
+        int row = 0;
+        std::string line;
+        while (std::getline(infile, line)) {
+            std::istringstream ss(line);
+            std::string value;
+            int col = 0;
+            while (std::getline(ss, value, ',')) {
+                matrix(row, col) = std::stof(value);
+                col++;
+            }
+            row++;
+        }
+        infile.close();
+    } else {
+        std::cerr << "Cannot open file: " << filename << std::endl;
+    }
+
+    return matrix;
+}
+
+cv::Point3d transformPoint(const cv::Point3d &point, const Eigen::Matrix4f &transformation) {
+    Eigen::Vector4f eigenPoint = Eigen::Vector4f((float)point.x, (float)point.y, (float)point.z, 1.0f);
+    Eigen::Vector4f transformedPoint = transformation * eigenPoint;
+    return cv::Point3d((double)transformedPoint(0), (double)transformedPoint(1), (double)transformedPoint(2));
+}
+
+std::vector<cv::Point3d> loadPoints() {
+    std::ifstream pointData;
+    std::vector<std::string> row;
+    std::string line, word, temp;
+
+    std::string settingPath = Auxiliary::GetGeneralSettingsPath();
+    std::ifstream programData(settingPath);
+    nlohmann::json data;
+    programData >> data;
+    programData.close();
+
+    std::string cloud_points = std::string(data["mapInputDir"]) + "cloud1.csv";
+
+    std::vector<cv::Point3d> points;
+
+    pointData.open(cloud_points, std::ios::in);
+
+    while (!pointData.eof()) {
+        row.clear();
+
+        std::getline(pointData, line);
+
+        std::stringstream words(line);
+
+        if (line == "") {
+            continue;
+        }
+
+        while (std::getline(words, word, ',')) {
+            try
+            {
+                std::stod(word);
+            }
+            catch(std::out_of_range)
+            {
+                word = "0";
+            }
+            row.push_back(word);
+        }
+        points.push_back(cv::Point3d(std::stod(row[0]), std::stod(row[1]), std::stod(row[2])));
+    }
+    pointData.close();
+
+    return points;
+}
+
+// Function to read CSV file into a vector of cv::Point3d
+std::vector<cv::Point3d> readPointsFromCSV(const std::string& filePath) {
+    std::vector<cv::Point3d> points;
+    std::ifstream file(filePath);
+    std::string line;
+    double x, y, z;
+
+    while (std::getline(file, line)) {
+        std::stringstream lineStream(line);
+        std::string cell;
+
+        std::getline(lineStream, cell, ',');
+        x = std::stod(cell);
+
+        std::getline(lineStream, cell, ',');
+        y = std::stod(cell);
+
+        std::getline(lineStream, cell, ',');
+        z = std::stod(cell);
+
+        points.emplace_back(x, y, z);
+    }
+
+    return points;
+}
+
+int main(int argc, char **argv) {
+    std::string settingPath = Auxiliary::GetGeneralSettingsPath();
+    std::ifstream programData(settingPath);
+    nlohmann::json data;
+    programData >> data;
+    programData.close();
+
+    std::string configPath = data["DroneYamlPathSlam"];
+    cv::FileStorage fSettings(configPath, cv::FileStorage::READ);
+
+    float fx = fSettings["Camera.fx"];
+    float fy = fSettings["Camera.fy"];
+    float cx = fSettings["Camera.cx"];
+    float cy = fSettings["Camera.cy"];
+    float viewpointX = fSettings["RunModel.ViewpointX"];
+    float viewpointY = fSettings["RunModel.ViewpointY"];
+    float viewpointZ = fSettings["RunModel.ViewpointZ"];
+
+    Eigen::Matrix3d K;
+    K << fx, 0.0, cx, 0.0, fy, cy, 0.0, 0.0, 1.0;
+    cv::Mat K_cv = (cv::Mat_<float>(3, 3) << fx, 0.0, cx, 0.0, fy, cy, 0.0, 0.0, 1.0);
+    Eigen::Vector2i viewport_desired_size(640, 480);
+
+    cv::Mat img;
+
+    // Options
+    bool show_bounds = false;
+    bool show_axis = false;
+    bool show_x0 = false;
+    bool show_y0 = false;
+    bool show_z0 = false;
+    bool cull_backfaces = false;
+
+    // Create Window for rendering
+    pangolin::CreateWindowAndBind("Main", viewport_desired_size[0], viewport_desired_size[1]);
+    glEnable(GL_DEPTH_TEST);
+
+    // Define Projection and initial ModelView matrix
+    pangolin::OpenGlRenderState s_cam(
+            pangolin::ProjectionMatrix(viewport_desired_size(0), viewport_desired_size(1), K(0, 0), K(1, 1), K(0, 2), K(1, 2), NEAR_PLANE, FAR_PLANE),
+            pangolin::ModelViewLookAt(viewpointX, viewpointY, viewpointZ, 0, 0, 0, 0.0, -1.0, pangolin::AxisY)
+    );
+
+    // Create Interactive View in window
+    pangolin::Handler3D handler(s_cam);
+    pangolin::View &d_cam = pangolin::CreateDisplay()
+            .SetBounds(0.0, 1.0, 0.0, 1.0, ((float)-viewport_desired_size[0] / (float)viewport_desired_size[1]))
+            .SetHandler(&handler);
+
+    // Load Geometry asynchronously
+    std::string model_path = data["modelPath"];
+    const pangolin::Geometry geom_to_load = pangolin::LoadGeometry(model_path);
+    auto aabb = pangolin::GetAxisAlignedBox(geom_to_load);
+    Eigen::AlignedBox3f total_aabb;
+    total_aabb.extend(aabb);
+    const auto mvm = pangolin::ModelViewLookAt(viewpointX, viewpointY, viewpointZ, 0, 0, 0, 0.0, -1.0, pangolin::AxisY);
+    const auto proj = pangolin::ProjectionMatrix(viewport_desired_size(0), viewport_desired_size(1), K(0, 0), K(1, 1), K(0, 2), K(1, 2), NEAR_PLANE, FAR_PLANE);
+    s_cam.SetModelViewMatrix(mvm);
+    s_cam.SetProjectionMatrix(proj);
+    const pangolin::GlGeometry geomToRender = pangolin::ToGlGeometry(geom_to_load);
+    // Render tree for holding object position
+    pangolin::GlSlProgram default_prog;
+    auto LoadProgram = [&]() {
+        default_prog.ClearShaders();
+        default_prog.AddShader(pangolin::GlSlAnnotatedShader, pangolin::shader);
+        default_prog.Link();
+    };
+    LoadProgram();
+    pangolin::RegisterKeyPressCallback('b', [&]() { show_bounds = !show_bounds; });
+    pangolin::RegisterKeyPressCallback('0', [&]() { cull_backfaces = !cull_backfaces; });
+
+    // Show axis and axis planes
+    pangolin::RegisterKeyPressCallback('a', [&]() { show_axis = !show_axis; });
+    pangolin::RegisterKeyPressCallback('x', [&]() { show_x0 = !show_x0; });
+    pangolin::RegisterKeyPressCallback('y', [&]() { show_y0 = !show_y0; });
+    pangolin::RegisterKeyPressCallback('z', [&]() { show_z0 = !show_z0; });
+
+    cv::Mat Twc;
+    bool use_lab_icp = bool(data["useLabICP"]);
+    std::cout << use_lab_icp << std::endl;
+
+    std::string transformation_matrix_csv_path;
+    if (use_lab_icp)
+    {
+        transformation_matrix_csv_path = std::string(data["framesOutput"]) + "frames_lab_transformation_matrix.csv";
+    }
+    else
+    {
+        transformation_matrix_csv_path = std::string(data["framesOutput"]) + "frames_transformation_matrix.csv";
+    }
+    Eigen::Matrix4f transformation = loadMatrixFromFile(transformation_matrix_csv_path);
+    std::cout << transformation << std::endl;
+
+    std::vector<cv::Point3d> points_to_draw;
+    std::vector<cv::Point3d> points = loadPoints();
+
+    for (auto point : points) {
+        points_to_draw.push_back(transformPoint(point, transformation));
+    }
+
+    // Read points from multiple CSV files in the directory
+    std::vector<cv::Point3d> cloudPoints1;
+    std::string orbs_csv_dir = data["orbFrames"];
+    for (const auto& entry : std::filesystem::directory_iterator(orbs_csv_dir)) {
+        if (entry.path().extension() == ".csv") {
+            std::vector<cv::Point3d> points = readPointsFromCSV(entry.path());
+            cloudPoints1.insert(cloudPoints1.end(), points.begin(), points.end());
+        }
+    }
+
+    cv::VideoWriter writer;
+    writer.open("./data.avi", cv::VideoWriter::fourcc('M', 'J', 'P', 'G'), 30.0, cv::Size(viewport_desired_size[0], viewport_desired_size[1]), true);
+
+    Eigen::Vector3d Pick_w = handler.Selected_P_w();
+    std::vector<Eigen::Vector3d> Picks_w;
+
+    while (!pangolin::ShouldQuit()) {
+        if ((handler.Selected_P_w() - Pick_w).norm() > 1E-6) {
+            Pick_w = handler.Selected_P_w();
+            Picks_w.push_back(Pick_w);
+            std::cout << pangolin::FormatString("\"Translation\": [%,%,%]", Pick_w[0], Pick_w[1], Pick_w[2])
+                      << std::endl;
+        }
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // Load any pending geometry to the GPU.
+        if (d_cam.IsShown()) {
+            d_cam.Activate();
+
+            if (cull_backfaces) {
+                glEnable(GL_CULL_FACE);
+                glCullFace(GL_BACK);
+            }
+            default_prog.Bind();
+            default_prog.SetUniform("KT_cw",  s_cam.GetProjectionMatrix() *  s_cam.GetModelViewMatrix());
+            pangolin::GlDraw( default_prog, geomToRender, nullptr);
+            default_prog.Unbind();
+
+            s_cam.Apply();
+
+            glDisable(GL_CULL_FACE);
+
+            drawPoints(cloudPoints1, points_to_draw);
+
+            int viewport_size[4];
+            glGetIntegerv(GL_VIEWPORT, viewport_size);
+
+            pangolin::Image<unsigned char> buffer;
+            pangolin::VideoPixelFormat fmt = pangolin::VideoFormatFromString("RGB24");
+            buffer.Alloc(viewport_size[2], viewport_size[3], viewport_size[2] * fmt.bpp / 8);
+            glReadBuffer(GL_BACK);
+            glPixelStorei(GL_PACK_ALIGNMENT, 1);
+            glReadPixels(0, 0, viewport_size[2], viewport_size[3], GL_RGB, GL_UNSIGNED_BYTE, buffer.ptr);
+
+            cv::Mat imgBuffer = cv::Mat(viewport_size[3], viewport_size[2], CV_8UC3, buffer.ptr);
+            cv::flip(imgBuffer, imgBuffer, 0);
+
+            writer.write(imgBuffer);
+        }
+
+        pangolin::FinishFrame();
+    }
+
+    writer.release();
+
+    return 0;
+}
