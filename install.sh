f0ecd2648478237697ad95bdb16b7f76081218ae
tzuk <tzuk9800@gmail.com>
2023-07-19 10:27:10 +0300

    added vcpkg script for linux as well and qt support for pangolin also added an updated install script

--- install.sh
+++ install.sh
@@ -1,23 +1,5 @@
 #!/bin/bash
 cwd=$(pwd)
-echo "Configuring and building Thirdparty/DBoW2 ..."
-sudo apt install libeigen3-dev
-cd Thirdparty/DBoW2
-mkdir build
-cd build
-
-cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j$(nproc)
-
-cd ../../g2o
-
-echo "Configuring and building Thirdparty/g2o ..."
-
-mkdir build
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j$(nproc)
-echo "Configuring and building Thirdparty/Pangolin ..."
 sudo apt-get install libncurses5-dev
 sudo apt-get install libglew-dev
 sudo apt-get install libglu1-mesa-dev freeglut3-dev mesa-common-dev
@@ -27,12 +9,13 @@ sudo apt-get install libjpeg-dev libpng-dev libtiff5-dev libopenexr-dev
 sudo apt-get install libboost-all-dev libopenblas-dev
 sudo apt-get install libbluetooth-dev
 sudo apt install libpcl-dev
-
-#cd ../../Pangolin
-#mkdir build
-#cd build
-#cmake -DCPP11_NO_BOOST=1 ..
-#sudo make -j$(nproc) install
+sudo apt -y install libxext-dev libxfixes-dev libxrender-dev libxcb1-dev libx11-xcb-dev libxcb-glx0-dev
+sudo apt -y install libxkbcommon-dev libxcb-keysyms1-dev libxcb-image0-dev libxcb-shm0-dev libxcb-icccm4-dev libxcb-sync0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-randr0-dev libxcb-render-util0-dev
+chmod +x opencv3.4.16Install.sh
+bash opencv3.4.16Install.sh
+cd Thirdparty
+rm -rf Pangolin
+git clone https://github.com/tzukpolinsky/Pangolin.git
 
 cd ~
 git clone https://github.com/nlohmann/json.git
@@ -49,20 +32,6 @@ cd build
 cmake ..
 sudo make -j$(nproc) install
 cd ~
-git clone https://github.com/gabime/spdlog.git
-cd spdlog
-mkdir build
-cd build
-cmake ..
-sudo make -j$(nproc) install
-cd ~
-git clone https://github.com/tzukpolinsky/ctello.git
-cd ctello
-mkdir build
-cd build
-cmake ..
-sudo make -j$(nproc) install
-cd ~
 git clone https://gitlab.com/libeigen/eigen.git
 cd eigen
 git checkout 3.4.0
@@ -71,5 +40,7 @@ cd build
 cmake ..
 sudo make -j$(nproc) install
 cd "$cwd"
-chmod +x build.sh
-./build.sh
+mkdir build
+cd build
+cmake ..
+make -j
