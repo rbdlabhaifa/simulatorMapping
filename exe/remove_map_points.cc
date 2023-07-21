0cc61c14fe9c49095e26ce045736f4a816a668fc
liamvanunu <liamvanunu1231@gmail.com>
2023-07-21 12:14:00 +0300

    Added two new scripts, removed redundant scripts
    Signed-off-by: liamvanunu <liamvanunu1231@gmail.com>

--- exe/remove_map_points.cc
+++ exe/remove_map_points.cc
@@ -1,3 +1,49 @@
-//
-// Created by liam on 7/18/23.
-//
+#include <memory>
+#include <string>
+#include <thread>
+#include <iostream>
+#include <unistd.h>
+#include <unordered_set>
+#include <nlohmann/json.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>
+
+#include "System.h"
+#include "Converter.h"
+#include "include/Auxiliary.h"
+
+int main() {
+    std::string settingPath = Auxiliary::GetGeneralSettingsPath();
+    std::ifstream programData(settingPath);
+    nlohmann::json data;
+    programData >> data;
+    programData.close();
+
+    char time_buf[21];
+    time_t now;
+    std::time(&now);
+    std::strftime(time_buf, 21, "%Y-%m-%d_%H:%S:%MZ", gmtime(&now));
+    std::string currentTime(time_buf);
+    std::string vocPath = data["VocabularyPath"];
+    std::string droneYamlPathSlam = data["DroneYamlPathSlam"];
+    std::string videoPath = data["offlineVideoTestPath"];
+    bool loadMap = data["loadMap"];
+    bool isSavingMap = data["saveMap"];
+    std::string loadMapPath = data["loadMapPath"];
+    ORB_SLAM2::System SLAM = ORB_SLAM2::System(vocPath, droneYamlPathSlam, ORB_SLAM2::System::MONOCULAR, true, true, true,
+                                               "/home/liam/simulatorMap.bin", false);
+
+    for (int i=0; i < SLAM.GetMap()->GetAllMapPoints().size(); i ++) {
+        SLAM.GetMap()->EraseMapPoint(SLAM.GetMap()->GetAllMapPoints()[i]);
+    }
+
+    std::cout << "Points left in map: " << SLAM.GetMap()->GetAllMapPoints().size() << std::endl;
+    SLAM.SaveMap("/home/liam/a.bin");
+
+    sleep(4);
+
+    SLAM.Shutdown();
+    cvDestroyAllWindows();
+
+    return 0;
+}
---
+++ exe/save_first_frame.cc
@@ -0,0 +1,45 @@
+#include "include/simulator.h"
+
+void handleKeyboardEventsScan(Simulator *simulator) {
+    pangolin::RegisterKeyPressCallback('l', [&simulator]() { simulator->ToggleFollowCamera(); });
+    pangolin::RegisterKeyPressCallback('\t', [&simulator]() { simulator->ToggleShowPoints(); });
+    pangolin::RegisterKeyPressCallback('i', [&simulator]() { simulator->DoReset(); });
+    pangolin::RegisterKeyPressCallback('a', [&simulator]() { simulator->MoveLeft(); });
+    pangolin::RegisterKeyPressCallback('d', [&simulator]() { simulator->MoveRight(); });
+    pangolin::RegisterKeyPressCallback('f', [&simulator]() { simulator->MoveDown(); });
+    pangolin::RegisterKeyPressCallback('r', [&simulator]() { simulator->MoveUp(); });
+    pangolin::RegisterKeyPressCallback('s', [&simulator]() { simulator->MoveBackward(); });
+    pangolin::RegisterKeyPressCallback('w', [&simulator]() { simulator->MoveForward(); });
+    pangolin::RegisterKeyPressCallback('q', [&simulator]() { simulator->RotateLeft(); });
+    pangolin::RegisterKeyPressCallback('e', [&simulator]() { simulator->RotateRight(); });
+    pangolin::RegisterKeyPressCallback('t', [&simulator]() { simulator->RotateDown(); });
+    pangolin::RegisterKeyPressCallback('g', [&simulator]() { simulator->RotateUp(); });
+    pangolin::RegisterKeyPressCallback('S', [&simulator]() { simulator->FinishScan(); });
+}
+
+void handleKeyboardEventsResult(Simulator *simulator) {
+    pangolin::RegisterKeyPressCallback('S', [&simulator]() { simulator->FinishScan(); });
+}
+
+int main(int argc, char **argv) {
+    Simulator simulator = Simulator();
+
+    handleKeyboardEventsScan(&simulator);
+    simulator.Run();
+
+    // Process cloud point generated
+    std::vector<OfflineMapPoint*> cloudPoints = simulator.GetCloudPoint();
+    std::cout << "Number of points: " << cloudPoints.size() << std::endl;
+
+    std::string fileLocation = "~/firstFramePoints.csv";
+
+    std::cout << "Saving points to " << fileLocation << std::endl;
+
+    std::ofstream pointsData(fileLocation);
+    for (auto &point : cloudPoints) {
+        pointData << point->point.x << point->point.y << point->point.z << std::endl;
+    }
+    pointsData.close();
+
+    return 0;
+}
