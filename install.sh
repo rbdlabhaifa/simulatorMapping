63b44d0729502c940cf2215fff733d0bed202b87
liamvanunu <liamvanunu1231@gmail.com>
2023-07-25 22:07:14 +0300

    Fix install scripts and bug fix
    Signed-off-by: liamvanunu <liamvanunu1231@gmail.com>

--- install.sh
+++ install.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 cwd=$(pwd)
-sudo apt install -y libeigen3-dev
+
+sudo apt-get install -y python3-numpy
+sudo ln -sf /usr/lib/python3/dist-packages/numpy/core/include/numpy /usr/include/numpy
+
+sudo apt-get install -y libncurses5-dev
+sudo apt-get install -y libglew-dev
+sudo apt-get install -y libglu1-mesa-dev freeglut3-dev mesa-common-dev
+sudo apt-get install -y ffmpeg libavcodec-dev libavutil-dev libavformat-dev libswscale-dev
+sudo apt-get install -y libdc1394-22-dev libraw1394-dev
+sudo apt-get install -y libjpeg-dev libpng-dev libtiff5-dev libopenexr-dev
+sudo apt-get install -y libboost-all-dev libopenblas-dev
+sudo apt-get install -y libbluetooth-dev
+sudo apt install -y libpcl-dev
+sudo apt -y install libxext-dev libxfixes-dev libxrender-dev libxcb1-dev libx11-xcb-dev libxcb-glx0-dev
+sudo apt -y install libxkbcommon-dev libxcb-keysyms1-dev libxcb-image0-dev libxcb-shm0-dev libxcb-icccm4-dev libxcb-sync0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-randr0-dev libxcb-render-util0-dev
+chmod +x opencv3.4.16Install.sh
+bash opencv3.4.16Install.sh
+
+sudo apt-get install -y libeigen3-dev
 cd ~
 git clone https://gitlab.com/libeigen/eigen.git
 cd eigen
@@ -18,20 +36,6 @@ if [ ! -f "/usr/local/include/Eigen" ]; then
   sudo ln -sf /usr/include/eigen3/unsupported/ /usr/include/unsupported
 fi
 cd "$cwd"
-
-sudo apt-get install -y libncurses5-dev
-sudo apt-get install -y libglew-dev
-sudo apt-get install -y libglu1-mesa-dev freeglut3-dev mesa-common-dev
-sudo apt-get install -y ffmpeg libavcodec-dev libavutil-dev libavformat-dev libswscale-dev
-sudo apt-get install -y libdc1394-22-dev libraw1394-dev
-sudo apt-get install -y libjpeg-dev libpng-dev libtiff5-dev libopenexr-dev
-sudo apt-get install -y libboost-all-dev libopenblas-dev
-sudo apt-get install -y libbluetooth-dev
-sudo apt install -y libpcl-dev
-sudo apt -y install libxext-dev libxfixes-dev libxrender-dev libxcb1-dev libx11-xcb-dev libxcb-glx0-dev
-sudo apt -y install libxkbcommon-dev libxcb-keysyms1-dev libxcb-image0-dev libxcb-shm0-dev libxcb-icccm4-dev libxcb-sync0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-randr0-dev libxcb-render-util0-dev
-chmod +x opencv3.4.16Install.sh
-bash opencv3.4.16Install.sh
 
 cd ~
 git clone https://github.com/nlohmann/json.git
--- opencv3.4.16Install.sh
+++ opencv3.4.16Install.sh
@@ -1,23 +1,23 @@
 sudo apt update
-sudo apt install build-essential cmake git pkg-config libgtk-3-dev \
+sudo apt install -y build-essential cmake git pkg-config libgtk-3-dev \
     libavcodec-dev libavformat-dev libswscale-dev libv4l-dev \
     libxvidcore-dev libx264-dev libjpeg-dev libpng-dev libtiff-dev \
     gfortran openexr libatlas-base-dev python3-dev python3-numpy \
     libtbb2 libtbb-dev libdc1394-22-dev libopenexr-dev \
     libgstreamer-plugins-base1.0-dev libgstreamer1.0-dev
 
-sudo apt-get install libprotobuf-dev protobuf-compiler
-sudo apt-get install libgoogle-glog-dev libgflags-dev
-sudo apt-get install libgphoto2-dev libeigen3-dev libhdf5-dev doxygen
-sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev
-sudo apt-get install libxine2-dev libv4l-dev
-sudo apt-get install libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev
-sudo apt-get install qt5-default libgtk2.0-dev libtbb-dev
-sudo apt-get install libatlas-base-dev
-sudo apt-get install libfaac-dev libmp3lame-dev libtheora-dev
-sudo apt-get install libvorbis-dev libxvidcore-dev
-sudo apt-get install libopencore-amrnb-dev libopencore-amrwb-dev
-sudo apt-get install x264 v4l-utils
+sudo apt-get install -y libprotobuf-dev protobuf-compiler
+sudo apt-get install -y libgoogle-glog-dev libgflags-dev
+sudo apt-get install -y libgphoto2-dev libeigen3-dev libhdf5-dev doxygen
+sudo apt-get install -y libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev
+sudo apt-get install -y libxine2-dev libv4l-dev
+sudo apt-get install -y libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev
+sudo apt-get install -y qt5-default libgtk2.0-dev libtbb-dev
+sudo apt-get install -y libatlas-base-dev
+sudo apt-get install -y libfaac-dev libmp3lame-dev libtheora-dev
+sudo apt-get install -y libvorbis-dev libxvidcore-dev
+sudo apt-get install -y libopencore-amrnb-dev libopencore-amrwb-dev
+sudo apt-get install -y x264 v4l-utils
 cd ~
 git clone https://github.com/opencv/opencv.git
 git clone https://github.com/opencv/opencv_contrib.git
