4f6dbf84bf7040027a6d6a6d4ae64f3c12b42e98
liamvanunu <liamvanunu1231@gmail.com>
2023-08-19 14:55:12 +0300

    Changed run model to only run model
    Signed-off-by: liamvanunu <liamvanunu1231@gmail.com>

--- exe/run_model.cc
+++ exe/run_model.cc
@@ -25,69 +25,6 @@
 #define NEAR_PLANE 0.1
 #define FAR_PLANE 20
 
-void drawPoints(std::vector<cv::Point3d> seen_points, std::vector<cv::Point3d> new_points_seen) {
-    std::string settingPath = Auxiliary::GetGeneralSettingsPath();
-    std::ifstream programData(settingPath);
-    nlohmann::json data;
-    programData >> data;
-    programData.close();
-
-    const int point_size = data["pointSize"];
-
-    glPointSize(point_size);
-    glBegin(GL_POINTS);
-    glColor3f(0.0, 0.0, 0.0);
-
-    for (auto point: seen_points) {
-        glVertex3f((float) (point.x), (float) (point.y), (float) (point.z));
-    }
-    glEnd();
-
-    glPointSize(point_size);
-    glBegin(GL_POINTS);
-    glColor3f(1.0, 0.0, 0.0);
-
-    for (auto point: new_points_seen) {
-        glVertex3f((float) (point.x), (float) (point.y), (float) (point.z));
-    }
-    std::cout << new_points_seen.size() << std::endl;
-
-    glEnd();
-}
-
-cv::Point3f convert2Dto3D(cv::Point2f keypoint, const cv::Mat& K, const cv::Mat& depth, const pangolin::OpenGlRenderState& cam_state) {
-    GLint viewport[4];
-    GLdouble modelview[16];
-    GLdouble projection[16];
-
-    glGetIntegerv(GL_VIEWPORT, viewport);
-    for (int i = 0; i < 16; ++i) {
-        modelview[i] = cam_state.GetModelViewMatrix().m[i];
-        projection[i] = cam_state.GetProjectionMatrix().m[i];
-    }
-
-    GLdouble x, y, z;
-    GLdouble worldX, worldY, worldZ;
-
-    x = keypoint.x;
-    y = (float)viewport[3] - keypoint.y; // OpenGL has the origin in the lower-left corner, so we need to flip the y-coordinate
-    z = depth.at<float>(static_cast<int>(y), static_cast<int>(x)); // Get depth value at the keypoint position
-
-    gluUnProject(x, y, z, modelview, projection, viewport, &worldX, &worldY, &worldZ);
-
-    return cv::Point3f((float)worldX, (float)worldY, (float)worldZ);
-}
-
-void saveKeypointsToCSV(const std::vector<cv::Point3d>& keypoints, const std::string& filename) {
-    std::ofstream csv_file(filename);
-
-    for (const auto& keypoint : keypoints) {
-        csv_file << keypoint.x << "," << keypoint.y << "," << keypoint.z << std::endl;
-    }
-
-    csv_file.close();
-}
-
 int main(int argc, char **argv) {
     std::string settingPath = Auxiliary::GetGeneralSettingsPath();
     std::ifstream programData(settingPath);
@@ -202,63 +139,9 @@ int main(int argc, char **argv) {
             int viewport_size[4];
             glGetIntegerv(GL_VIEWPORT, viewport_size);
 
-            pangolin::Image<unsigned char> buffer;
-            pangolin::VideoPixelFormat fmt = pangolin::VideoFormatFromString("RGBA32");
-            buffer.Alloc(viewport_size[2], viewport_size[3], viewport_size[2] * fmt.bpp/8 );
-            glReadBuffer(GL_BACK);
-            glPixelStorei(GL_PACK_ALIGNMENT, 1);
-            glReadPixels(0, 0, viewport_size[2], viewport_size[3], GL_RGBA, GL_UNSIGNED_BYTE, buffer.ptr);
-
-            cv::Mat  imgBuffer = cv::Mat(viewport_size[3], viewport_size[2], CV_8UC4, buffer.ptr);
-            cv::cvtColor(imgBuffer, img,  cv::COLOR_RGBA2GRAY);
-            img.convertTo(img, CV_8UC1);
-            cv::flip(img, img, 0);
-
-            cv::imshow("image", img);
-            cv::waitKey(2); // You can replace 2 with 0 if you want the window to wait indefinitely for a key press
-
-            // Detect keypoints
-            std::vector<cv::KeyPoint> keypoints;
-            cv::Mat descriptors;
-            (*orbExtractor)(img, cv::Mat(), keypoints, descriptors);
-
-            // Draw keypoints on the image
-            cv::Mat image_keypoints;
-            cv::drawKeypoints(img, keypoints, image_keypoints);
-
-            cv::imshow("image_keypoints", image_keypoints);
-            cv::waitKey(2); // You can replace 2 with 0 if you want the window to wait indefinitely for a key press
-
-            // Save the x and y values of the keypoints to a vector
-            std::vector<cv::Point2f> keypoint_positions;
-            for (const auto& keypoint : keypoints)
-            {
-                cv::Point2f position = cv::Point2f((float)keypoint.pt.x, (float)(keypoint.pt.y));
-                keypoint_positions.push_back(position);
-            }
-
-            cv::Mat depth(viewport_size[3], viewport_size[2], CV_32FC1);
-            glReadPixels(0, 0, viewport_size[2], viewport_size[3], GL_DEPTH_COMPONENT, GL_FLOAT, depth.data);
-            
-            // Convert keypoints pixels to keypoints 3d points
-            std::vector<cv::Point3d> keypoint_points;
-            for (const auto& keypoint : keypoint_positions)
-            {
-                cv::Point3f point_float = convert2Dto3D(keypoint, K_cv, depth, s_cam);
-                cv::Point3d point = cv::Point3d(point_float.x, point_float.y, point_float.z);
-                keypoint_points.push_back(point);
-            }
-
-            int frame_to_check = data["frameNumber"];
-            std::string keypoints_csv_path = std::string(data["framesOutput"]) + "frame_" + std::to_string(frame_to_check) + "_orbs.csv";
-
-            saveKeypointsToCSV(keypoint_points, keypoints_csv_path);
-
             s_cam.Apply();
 
             glDisable(GL_CULL_FACE);
-
-            drawPoints(std::vector<cv::Point3d>(), keypoint_points);
         }
 
         pangolin::FinishFrame();
